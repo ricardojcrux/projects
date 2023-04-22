14f0408bd0708e4aa313951cd214b8183f2e4e8f
Ricardo Zapata <ricardozapatacruz@gmail.com>
2023-04-22 14:39:03 -0500

    Creacion de calculadora funcional y creación del archivo gitignore de defecto

--- calculadora.py
+++ calculadora.py
@@ -1,44 +1,160 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title('Calculadora Básica')
-window.geometry('720x480')
+valor,i,termino = 0,0,''
+z = 0
 
-etiq1=tkinter.Label(window, text='Entrada', bg='yellow' ,fg='red',font=('arial',20))
-etiq1.place(relx=0.1, y=20)
+def barrido(x):
+	global z,i
+	if z == 0:
+		window.delete(0,END)
 
-entry=tkinter.Entry()
-entry.place(relx=0.1, y=60)
+def funcion(x):
+	barrido(0)
+	global z
+	z += 1
+	window.insert(END,x)
 
-etiq2=tkinter.Label(window, text='Salida', bg='cyan', fg='blue',font=('arial',20))
-etiq2.place(relx=0.7, y=20)
+def limpiar():
+    global valor, i, termino, z
+    valor, i, z, termino = 0,0,0,''
+    window.delete(0,END)
+    window.insert(0,0)
+    
+def suma():
+	entrada = float(window.get())
+	global valor, i, termino, z
+	valor += entrada
+	window.delete(0,END)
+	i += 1
+	z += 1
+	termino = 'suma'
+    
+def resta():
+	entrada = float(window.get())
+	global valor, i, termino, z
+	if i == 0:
+		valor += entrada
+	else:
+		valor -= entrada
+	window.delete(0,END)
+	i += 1
+	z += 1
+	termino = 'resta'
+            
+def multiplicacion():
+	entrada = float(window.get())
+	global valor, i, termino, z
+	if i == 0:
+		valor += entrada
+	else:
+		valor *= entrada
+	window.delete(0,END)
+	i += 1
+	z += 1
+	termino = 'multiplicacion'
 
-exit= tkinter.Entry()
-exit.place(relx=0.7, y=60)
+def division():
+	entrada = float(window.get())
+	global valor, i, termino, z
+	if i == 0:
+		valor += entrada
+	else:
+		valor = valor * (1 / entrada)
+	window.delete(0,END)
+	i += 1
+	z += 1
+	termino = 'division'
 
-def Cuadrado():
-	exit.delete(0,tkinter.END)
-	ingreso = float(entry.get())
-	cuadrado = ingreso**2
-	exit.insert(0,cuadrado)
+def igual():
+	global termino, valor, i, z
+	if termino == '':
+		valor = int(window.get())
+		window.insert(0,valor)
+	elif termino == 'suma':
+		suma()
+	elif termino == 'resta':
+		resta()
+	elif termino == 'multiplicacion':
+		multiplicacion()
+	elif termino == 'division':
+		division()
+	window.delete(0,END)
+	tag = abs(valor) - abs(int(valor))
+	if tag == 0.0:
+		window.insert(0,round(valor))
+	else:
+		window.insert(0,round(valor,5))
+	valor,i,z = 0,0,0
 
-def Cubo():
-	exit.delete(0,tkinter.END)
-	ingreso = float(entry.get())
-	cuadrado = ingreso**3
-	exit.insert(0,cuadrado)
+calc = Tk()
+calc.title('Calculadora con Python')
+calc.geometry('400x750')
+calc.resizable(width=False, height=False)
 
-def Borrar():
-	entry.delete(0,tkinter.END)
-	exit.delete(0,tkinter.END)
+title=Label(calc, text='Calculadora', font=('Script MT Bold',40))
+title.pack(pady=10)
 
-boton = tkinter.Button(window, text='Cuadrado', bg='green', command=Cuadrado)
-boton.place(relx=0.1, y=100)
+window=Entry(calc)
+window.config(width=40, font=('led calculator',30), bg='black', fg='white' ,justify=RIGHT)
+window.insert(0,0)
+window.bind('<Button-1>',barrido)
+window.pack(padx=25, pady=10)
+    
+first = Frame(calc)
+second = Frame(calc)
+third = Frame(calc)
+last = Frame(calc)
+first.pack(pady=10)
+second.pack(pady=10)
+third.pack(pady=10)
+last.pack(pady=10)
 
-boton = tkinter.Button(window, text='Al Cubo', bg='blue', fg='white' ,command=Cubo)
-boton.place(relx=0.1, y=130)
+siete = Button(first, text='7',font=('Arial',40), height=1, width=2, command = lambda: funcion(7))
+siete.pack(padx=10,pady=10, side=LEFT)
 
-boton = tkinter.Button(window, text='Borrar Todo',bg='purple',fg='white',command=Borrar)
-boton.place(relx=0.1, y=160)
+ocho = Button(first, text='8',font=('Arial',40), height=1, width=2, command = lambda: funcion(8))
+ocho.pack(padx=10,pady=10, side=LEFT)
 
-window.mainloop()
+nueve = Button(first, text='9',font=('Arial',40), height=1, width=2, command = lambda: funcion(9))
+nueve.pack(padx=10,pady=10, side=LEFT)
+
+mas = Button(first, text='+',font=('Arial',40), height=1, width=2, command=suma)
+mas.pack(padx=10,pady=10, side=LEFT)
+
+cuatro = Button(second, text='4',font=('Arial',40), height=1, width=2, command = lambda: funcion(4))
+cuatro.pack(padx=10,pady=10, side=LEFT)
+
+cinco = Button(second, text='5',font=('Arial',40), height=1, width=2, command = lambda: funcion(5))
+cinco.pack(padx=10,pady=10, side=LEFT)
+
+seis = Button(second, text='6',font=('Arial',40), height=1, width=2, command = lambda: funcion(6))
+seis.pack(padx=10,pady=10, side=LEFT)
+
+menos = Button(second, text='-',font=('Arial',40), height=1, width=2,command=resta)
+menos.pack(padx=10,pady=10, side=LEFT)
+
+uno = Button(third, text='1',font=('Arial',40), height=1, width=2, command = lambda: funcion(1))
+uno.pack(padx=10,pady=10, side=LEFT)
+
+dos = Button(third, text='2',font=('Arial',40), height=1, width=2, command = lambda: funcion(2))
+dos.pack(padx=10,pady=10, side=LEFT)
+
+tres = Button(third, text='3',font=('Arial',40), height=1, width=2, command = lambda: funcion(3))
+tres.pack(padx=10,pady=10, side=LEFT)
+
+por = Button(third, text='×',font=('Arial',40), height=1, width=2,command=multiplicacion)
+por.pack(padx=10,pady=10, side=LEFT)
+
+clear = Button(last, text='C',font=('Arial',40), height=1, width=2,command=limpiar)
+clear.pack(padx=10,pady=10, side=LEFT)
+
+cero = Button(last, text='0',font=('Arial',40), height=1, width=2, command = lambda: funcion(0))
+cero.pack(padx=10,pady=10, side=LEFT)
+
+igual = Button(last, text='=',font=('Arial',40), height=1, width=2, command= igual)
+igual.pack(padx=10,pady=10, side=LEFT)
+
+entre = Button(last, text='÷',font=('Arial',40), height=1, width=2, command= division)
+entre.pack(padx=10,pady=10, side=LEFT)
+
+calc.mainloop()
